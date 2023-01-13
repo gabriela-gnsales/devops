b61736c79fc8f6302555bfd747fd1972bc065539
gabriela-gnsales <gnsales.gabriela@gmail.com>
2023-01-13 17:11:48 -0300

    :sparkles: Adicionando conteúdo do módulo de conteinerização

--- 05_conteinerização/docker-compose/03/src/gritando.py
+++ 05_conteinerização/docker-compose/03/src/gritando.py
@@ -1,7 +1,12 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/')
 def index():
-    return '<h1>Gritando na tela!</h1>'
+    return '<h1>Gritando na tela uhuul!</h1>'
+
+@app.route('/boanoite')
+def boa_noite():
+    return '<h1>Não durmam antes do fim da aula!</h1>'
